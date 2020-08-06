156d60d7a48f4bf30a58ecc6463da6ef1bbb85d6
Alexander Ursu <alexander.ursu@gmail.com>
2020-08-06 09:56:40 +0200

    Added class pxe::enc

---
+++ manifests/enc.pp
@@ -0,0 +1,23 @@
+# @summary Puppet ENC repository setup
+#
+# Fetch Puppet ENC repository setup to PXE server
+#
+# @example
+#   include pxe::enc
+class pxe::enc (
+  String  $repo_source,
+  String  $repo_revision  = 'master',
+  Optional[Stdlib::Unixpath]
+          $repo_identity  = undef,
+){
+  include pxe::storage
+
+  vcsrepo { '/var/lib/pxe/enc':
+    ensure     => latest,
+    provider   => 'git',
+    source     => $repo_source, # eg git@git.domain.tld:project/enc.git
+    revision   => $repo_revision,
+    submodules => false,
+    identity   => $repo_identity,
+  }
+}
