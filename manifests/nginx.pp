6582a332bd5d17de4eecf1b908ba3970a01d1e27
Alexander Ursu <alexander.ursu@gmail.com>
2020-08-12 16:53:45 +0200

    Added simple Nginx proxy for PXE server

---
+++ manifests/nginx.pp
@@ -0,0 +1,39 @@
+# @summary A short summary of the purpose of this class
+#
+# A description of what this class does
+#
+# @example
+#   include pxe::nginx
+class pxe::nginx (
+  Stdlib::Fqdn
+          $server_name,
+  Array[Stdlib::IP::Address]
+          $resolver,
+)
+{
+  $location_proxy_handler = {
+    proxy_set_header  => [
+      'Host $host',
+      'X-Forwarded-For $proxy_add_x_forwarded_for',
+    ],
+    proxy_pass_header => [
+      'Server'
+    ],
+    proxy             => "http://\$server_addr:8080",  # lint:ignore:variables_not_enclosed
+  }
+
+  nginx::resource::server { 'pxe':
+    server_name => [ $server_name ],
+
+    error_log   => '/var/log/nginx/pxe.error_log info',
+    access_log  => {
+      '/var/log/nginx/pxe.access_log' => 'combined',
+    },
+
+    resolver    => $resolver,
+
+    locations   => {
+      'pxe-default' => { location  => '/' } + $location_proxy_handler,
+    }
+  }
+}
