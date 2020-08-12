0a723dcd62060b0a817ebcad2cdfb31b1dcfe2bb
Alexander Ursu <alexander.ursu@gmail.com>
2020-08-12 17:07:34 +0200

    Corrected spec test for pxe::nginx

--- spec/classes/nginx_spec.rb
+++ spec/classes/nginx_spec.rb
@@ -3,9 +3,17 @@
 require 'spec_helper'
 
 describe 'pxe::nginx' do
+  let(:pre_condition) { "class { 'nginx': }" }
+
   on_supported_os.each do |os, os_facts|
     context "on #{os}" do
       let(:facts) { os_facts }
+      let(:params) do
+        {
+          server_name: 'install.domain.tld',
+          resolver: ['192.168.1.1']
+        }
+      end
 
       it { is_expected.to compile }
     end
