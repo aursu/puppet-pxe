f8a743742096f2b53d70066c1bcb44798e2d498b
Alexander Ursu <alexanderu@crytek.de>
2025-01-28 22:45:41 +0100

    Corrected iPXE to support Ubuntu

--- manifests/ipxe.pp
+++ manifests/ipxe.pp
@@ -4,26 +4,41 @@
 #
 # @example
 #   include pxe::ipxe
-class pxe::ipxe {
+class pxe::ipxe inherits pxe::params {
   include pxe::storage
+  include bsys::params
+
+  $tftp_root = $pxe::params::tftp_directory
+  $ipxe_root = "${tftp_root}/boot/ipxe"
+  $ipxe_package = $pxe::params::ipxe_package
 
   # iPXE is an open source network bootloader
-  package { 'ipxe-bootimgs':
+  package { $ipxe_package:
     ensure  => 'present',
   }
-
-  $tftp_root = '/var/lib/tftpboot'
-  $ipxe_root = "${tftp_root}/boot/ipxe"
 
   file { $ipxe_root:
     ensure => directory,
   }
 
-  ['undionly.kpxe', 'ipxe.efi'].each |$ipxe_file| {
-    file { "${ipxe_root}/${ipxe_file}":
-      ensure  => file,
-      source  => "file:///usr/share/ipxe/${ipxe_file}",
-      require => Package['ipxe-bootimgs'],
+  case $bsys::params::osfam {
+    'RedHat': {
+      ['undionly.kpxe', 'ipxe.efi'].each |$ipxe_file| {
+        file { "${ipxe_root}/${ipxe_file}":
+          ensure  => file,
+          source  => "file:///usr/share/ipxe/${ipxe_file}",
+          require => Package[$ipxe_package],
+        }
+      }
+    }
+    'Debian': {
+      ['undionly.kpxe', 'ipxe.efi'].each |$ipxe_file| {
+        file { "${ipxe_root}/${ipxe_file}":
+          ensure  => file,
+          source  => "file:///usr/lib/ipxe/${ipxe_file}",
+          require => Package[$ipxe_package],
+        }
+      }
     }
   }
 
