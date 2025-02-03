a29a723174d4ac131f086f430d7abe7ccdc38655
Alexander Ursu <alexanderu@crytek.de>
2025-02-03 22:24:38 +0100

    Added unit tests for grub

--- spec/classes/grub_spec.rb
+++ spec/classes/grub_spec.rb
@@ -8,6 +8,18 @@ describe 'pxe::grub' do
       let(:facts) { os_facts }
 
       it { is_expected.to compile }
+
+      if os_facts[:os]['family'] == 'RedHat'
+        it {
+          is_expected.to contain_file('/var/lib/tftpboot/boot/grub/grub.cfg')
+            .with_content(%r{^set kernel='/boot/centos/10-stream/BaseOS/x86_64/os/images/pxeboot/vmlinuz'$})
+        }
+      elsif os_facts[:os]['family'] == 'Debian'
+        it {
+          is_expected.to contain_file('/srv/tftp/boot/grub/grub.cfg')
+            .with_content(%r{^set kernel='/boot/ubuntu/noble/casper/vmlinuz'$})
+        }
+      end
     end
   end
 end
